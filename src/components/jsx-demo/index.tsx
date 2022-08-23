593986bb51b102db2ecc32d33c00f5a29d80cc50
牛思顿 <1622598684@qq.com>
2022-08-23 11:26:39 +0800

    3d

---
+++ src/components/jsx-demo/index.tsx
@@ -0,0 +1,32 @@
+import { DefineComponent, defineComponent,ref, onMounted} from 'vue';
+import { noisyLinesBackground } from '../../../dist/esm/index'
+
+export interface Props {
+  count?: number;
+}
+
+/**
+ * JsxDemo.
+ */
+export const JsxDemo = defineComponent({
+  name: 'FileTabs',
+  inheritAttrs: true,
+  props: {
+    count: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+  },
+  setup(props: Props) {
+    const root = ref()
+    onMounted(() => {
+        noisyLinesBackground({
+          el: root.value
+        })
+    })
+    return () => (
+      <div ref={root}>count in jsx component: {props.count}</div>
+    );
+  },
+}) as DefineComponent<Props>;
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// // eslint-disable-next-line no-restricted-exports
+export { InstallPlugin as default } from './plugin';
+
+export * from './types'; // can not delete
+export * from './components';
+// export { default as neonCursor } from './cursors/neon/index';
+// export { default as particlesCursor } from './cursors/particles/index';
+// export { default as noisyLinesBackground } from './backgrounds/noisy-lines/index';
+// export { default as butterfliesBackground } from './backgrounds/butterflies/index';
+// export { default as swarmBackground } from './backgrounds/swarm/index';
+// export { default as fishesBackground } from './backgrounds/fishes/index';
